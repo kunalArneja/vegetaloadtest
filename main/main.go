bfa7e22092e7762430e948104932830e6e484000
Kunal Arneja <kunal.arneja@narvar.com>
2019-12-26 18:05:19 +0530

    added magento config to vageta

--- main/main.go
+++ main/main.go
@@ -1,56 +1,76 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/perf/config"
 	"github.com/perf/parser"
-  "time"
-  "github.com/perf/config"
-  "github.com/perf/utils"
-  vegeta "github.com/tsenart/vegeta/lib"
-  "flag"
+	"github.com/perf/utils"
+	vegeta "github.com/tsenart/vegeta/lib"
 )
 
 func main() {
-  var metrics vegeta.Metrics
+	var metrics vegeta.Metrics
 
-  //conf:= config.InitConfig("../shopify/orders-qa.yaml")
-  confArg := flag.String("config", "../config.yaml", "path to cofig yml")
-  flag.Parse()
-  fmt.Println(*confArg)
-  //panic(0)
-  conf:= config.InitConfig(*confArg)
-  
-  //refactor this into constants file
-  URL := conf.GetString("url")
-  HTTP_METHOD := conf.GetString("httpmethod")
-  VEGETA_RATE := conf.GetInt("rate")
-  DURATION := conf.GetInt("duration")
-  HEADERS := conf.GetStringMapString("static-headers")
-  DYNAMIC_HEADERS := conf.GetStringMap("dynamic-headers")
+	//conf:= config.InitConfig("../shopify/orders-qa.yaml")
+	confArg := flag.String("config", "../magento/shipment-config.yaml", "path to config yml")
+	flag.Parse()
+	fmt.Println(*confArg)
+	//panic(0)
+	conf := config.InitConfig(*confArg)
 
-  dynamic_headers := utils.ConvertToMapStringMapStringString(DYNAMIC_HEADERS)
+	//refactor this into constants file
+	URL := conf.GetString("url")
+	HTTP_METHOD := conf.GetString("httpmethod")
+	VEGETA_RATE := conf.GetInt("rate")
+	DURATION := conf.GetInt("duration")
+	HEADERS := conf.GetStringMapString("static-headers")
+	DYNAMIC_HEADERS := conf.GetStringMap("dynamic-headers")
 
+	dynamic_headers := utils.ConvertToMapStringMapStringString(DYNAMIC_HEADERS)
 
-  JSON_FILE_PATH := conf.GetString("post-request-json-file-path")
-  DYNAMIC_FIELDS := conf.GetStringMapString("post-request-json-dynamic-fields")
-  RESULTS_FILE_PATH := conf.GetString("dump-attack-results-file-path")
-  REQUESTS_FILE_PATH := conf.GetString("dump-request-file-path")
+	JSON_FILE_PATH := conf.GetString("post-request-json-file-path")
+	DYNAMIC_FIELDS := conf.GetStringMapString("post-request-json-dynamic-fields")
+	RESULTS_FILE_PATH := conf.GetString("dump-attack-results-file-path")
+	REQUESTS_FILE_PATH := conf.GetString("dump-request-file-path")
+	IDENTITY := conf.GetStringMapString("identity")
+	IDENTITY_SHIPMENT := conf.GetStringMapString("identity-shipment")
 
-  http_headers := utils.GetHttpHeaders(HEADERS)
-  test_rate := vegeta.Rate{Freq: VEGETA_RATE, Per: time.Second}
-  test_duration := time.Duration(DURATION) * (time.Second)
+	identityStart, exists := IDENTITY["start"]
+	identityIncr, _ := IDENTITY["increment"]
 
-  requestsFileWriter, _ := utils.OpenFileCreateIfNotFound(REQUESTS_FILE_PATH)
-  jsonString := parser.GetJsonString(JSON_FILE_PATH)
+	if exists {
+		parser.Counter, _ = strconv.Atoi(identityStart)
+		parser.CounterIncrement, _ = strconv.Atoi(identityIncr)
+	}
 
-  targeter := utils.GetTargeter(URL, HTTP_METHOD, http_headers, jsonString, DYNAMIC_FIELDS, requestsFileWriter, dynamic_headers)
-  attacker := vegeta.NewAttacker()
+	identityShipmentStart, exists := IDENTITY_SHIPMENT["start"]
+	identityShipmentIncr, _ := IDENTITY_SHIPMENT["increment"]
 
-  for res := range attacker.Attack(targeter, test_rate, test_duration, "Bang!") {
-    metrics.Add(res)
-  }
+	if exists {
+		parser.ShipmentCounter, _ = strconv.Atoi(identityShipmentStart)
+		parser.ShipmentCounterIncrement, _ = strconv.Atoi(identityShipmentIncr)
+	}
 
-  reporter := vegeta.NewJSONReporter(&metrics)
-  metrics.Close()
-  utils.ProcessReport(reporter, RESULTS_FILE_PATH)
+	http_headers := utils.GetHttpHeaders(HEADERS)
+	test_rate := vegeta.Rate{Freq: VEGETA_RATE, Per: time.Second}
+	test_duration := time.Duration(DURATION) * (time.Second)
+
+	requestsFileWriter, _ := utils.OpenFileCreateIfNotFound(REQUESTS_FILE_PATH)
+	jsonString := parser.GetJsonString(JSON_FILE_PATH)
+
+	targeter := utils.GetTargeter(URL, HTTP_METHOD, http_headers, jsonString, DYNAMIC_FIELDS, requestsFileWriter, dynamic_headers)
+	attacker := vegeta.NewAttacker()
+	vegeta.Timeout(2 * time.Hour)
+
+	for res := range attacker.Attack(targeter, test_rate, test_duration, "Bang!") {
+		metrics.Add(res)
+	}
+
+	reporter := vegeta.NewJSONReporter(&metrics)
+	metrics.Close()
+	utils.ProcessReport(reporter, RESULTS_FILE_PATH)
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,42 +1,66 @@
 package parser
 
 import (
-	"time"
-	"github.com/tidwall/sjson"
 	"io/ioutil"
-    "os"
-    "github.com/google/uuid"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/tidwall/sjson"
 )
 
+var Counter = 0
+var CounterIncrement = 0
+var ShipmentCounter = 0
+var ShipmentCounterIncrement = 0
 
-func GetJsonString(filePath string) string{
-    jsonFile, err := os.Open(filePath)
-    if err != nil {
-        panic(err)
-    }
-    defer jsonFile.Close()
+func GetJsonString(filePath string) string {
+	jsonFile, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer jsonFile.Close()
 
-    byteValue, _ := ioutil.ReadAll(jsonFile)
-    jsonString := string(byteValue[:])
-    
-    return jsonString
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	jsonString := string(byteValue[:])
+
+	return jsonString
 }
 
-func GetPreparedJsonForRequest(jsonString string, dynamicfields map[string]string) string  {
-    newJSON:= jsonString   
-
-    for key, value := range dynamicfields {
-        switch value{
-            case "timestamp":
-               newJSON, _ = sjson.Set(newJSON, key, time.Now().UTC().Format(time.RFC3339))
-               //fmt.Println(newJSON)
-            case "uuid":
-                newJSON, _= sjson.Set(newJSON, key , uuid.New().String())
-            case "epoch":
-                newJSON, _= sjson.Set(newJSON, key , time.Now().Unix())
-            case "epochnano":
-                newJSON, _= sjson.Set(newJSON, key , time.Now().UnixNano())
-        }
-    }
-    return newJSON
+func GetUrlAndBody(url string, jsonString string, dynamicfields map[string]string) (urlRet string, body string) {
+	newJSON := jsonString
+	attrValue := ""
+	for key, value := range dynamicfields {
+		switch value {
+		case "timestamp":
+			attrValue = time.Now().UTC().Format(time.RFC3339)
+			newJSON, _ = sjson.Set(newJSON, key, attrValue)
+		case "uuid":
+			attrValue = uuid.New().String()
+			newJSON, _ = sjson.Set(newJSON, key, attrValue)
+		case "epoch":
+			attrValue = strconv.FormatInt(time.Now().Unix(), 10)
+			newJSON, _ = sjson.Set(newJSON, key, attrValue)
+		case "epochnano":
+			attrValue = strconv.FormatInt(time.Now().UnixNano(), 10)
+			newJSON, _ = sjson.Set(newJSON, key, attrValue)
+		case "identity":
+			attrValue = strconv.Itoa(Counter)
+			Counter = Counter + CounterIncrement
+		case "replace":
+			attrValue = strconv.Itoa(ShipmentCounter)
+			searchString := "replace_" + key
+			if strings.Contains(newJSON, searchString) {
+				newJSON = strings.Replace(newJSON, searchString, attrValue, 1)
+			}
+			ShipmentCounter = ShipmentCounter + ShipmentCounterIncrement
+		}
+		searchString := "{" + key + "}"
+		if strings.Contains(url, searchString) {
+			url = strings.Replace(url, searchString, attrValue, 1)
+		}
+	}
+	return url, newJSON
 }
